873f290ba38d270c86e5f5211cf0104e5e2acee6
yustarg <yustar130@sina.com>
2017-02-05 22:48:59 +0800

    @add motionblur

---
+++ Assets/Shaders/Chapter5/MotionBlur.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MotionBlur : MonoBehaviour {
+
+    public Material mat;
+    private RenderTexture accumTexture;
+
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        if (accumTexture == null || accumTexture.width != src.width || accumTexture.height != src.height)
+        {
+            DestroyImmediate(accumTexture);
+            accumTexture = new RenderTexture(src.width, src.height, 0);
+            accumTexture.hideFlags = HideFlags.HideAndDontSave;
+            Graphics.Blit(src, accumTexture);
+        }
+        mat.SetTexture("_MainTex", src);
+        Graphics.Blit(src, accumTexture, mat);
+        Graphics.Blit(accumTexture, dest);
+    }
+}
